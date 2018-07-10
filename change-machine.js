6e48bc310bddcd78d599f0ca78e99e068c6236e9
Paul <paul.gaddy@csedge.org>
2018-07-10 12:57:22 -0400

    Change Machine
    Enter number

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,10 @@
 // Author: FirstName LastName
+var readline = require("readline-sync");
+var cents = readline.question("bLeep blOop! i'm a CHanGE macHINe! Please enter cents as a positive integer:  ");
+
+const q = Math.floor(cents / 25);
+		
+const d = Math.floor(cents / 10);
+const n = Math.floor(cents / 5);
+const p = Math.floor(cents / 1);
+console.log( "cents makes:" );
