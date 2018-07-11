e8e80fafb86e8ea2c42ef0623a8d881d5d71e0f9
Paul <paul.gaddy@csedge.org>
2018-07-11 15:54:10 -0400

    Coin Flip
    Done

---
+++ coin-flipper.js
@@ -0,0 +1,11 @@
+var readline = require("readline-sync");
+var coin = readline.question("Heads or tails ");
+
+var amt = console.log(Math.random());
+
+if(amt<0.5){
+		console.log("heads");
+}
+else{
+	console.log("tails");
+}
