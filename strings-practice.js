2a12b4321217e04fd5d68b79a8211592fed56929
Paul <paul.gaddy@csedge.org>
2018-07-13 11:59:16 -0400

    11:59 AM

---
+++ strings-practice.js
@@ -0,0 +1,27 @@
+// var string1 = "a" 
+// var string2 = "b"
+
+// function makeWord( a, b)
+// 	{
+// 		console.log("a" + "b" + "b" + "a");
+// 	}
+// 		makeWord();
+
+// var string1 = "a ";
+// var string2 = "b" ;
+
+// function comboString( a, b)
+// 	{
+// 		if( string1.length > string2.length || string2.length > string1.length )
+// 		{
+			
+// 		}
+// 		console.log( string1 + string2 );
+// 	}
+
+var string1 = "joe";
+var string2 = "by";
+
+string1.charAt(string1, string2);
+
+return();
---
+++ while-loops-practice.js
@@ -0,0 +1,54 @@
+// 1a. Use a while loop to print out numbers from 1 to 10
+// 1b. Use a while loop to print out numbers from 30 to 15
+let set1 = 1;
+while( set1 < 10)
+{
+	console.log(set1);
+	set1 += 1;
+}
+
+let set2 = 15;
+while( set2 > 30)
+	{
+		console.log(set1);
+		set += 1;
+	}
+// 2. Write a program that keeps rolling a die until the roll is a 5. Print out each roll.
+// If you haven't yet done so, you should run
+// npm install random-int
+// and use that to get a random number from 1 to 6.
+// Google "npm random-int" for more info.
+// var randomInt = require('random-int');
+var die;
+while( die < 5)
+	{
+		randomInt(1,5);
+		console.log(randomInt);
+	}
+
+// 3. Write a program that asks the user to guess a secret password.
+// Until the user enters the correct password (which is chosen by you in advance),
+// the program will keep asking for all eternity. When they finally do enter the
+// correct password, exit the loop and print a congratulatory message.
+
+var readline = require("readline-sync");
+var password = "draven";
+var passwordEntered = readline.question("enter the password ");
+
+while(passwordEntered == password)
+	{
+		
+	}
+if( passwordEntered = password )
+{
+	console.log("thats the password");
+}
+// Hint: Declare userInput outside the loop and initialize it to the value of readline.question("Enter password: ").
+// The while loop will check if userInput matches the correct password.
+// If not, inside the loop you should print "Sorry! That's incorrect", then
+// assign userInput to the value of readline.question("Enter password: ") again.
+
+// Bonus: Enhance your program to only allow the user to try entering the correct password three times.
+// If they fail three times, the program should kick them out and tell them they are a robot.
+// When the user enters the wrong password, the program tells them that they are wrong,
+// along with how many attempts they've made so far.
